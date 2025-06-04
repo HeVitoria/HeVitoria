7db273b3753f3bf3e5c19d115a9c08d589834fb4
HeVitoria <helena.simao.moreira@escola.pr.gov.br>
2025-06-04 16:40:40 -0300

    Add files via upload

---
+++ Helena 2ºB/perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Gatos',
+    'Qual é o nome científico do gato doméstico?',
+    'Felis catus'
+)
+
+criaCartao(
+    'Gatos',
+    'Que órgão sensorial os gatos usam, além dos olhos, para sentir as vibrações ao seu redor??',
+    'Os bigodes'
+)
+
+criaCartao(
+    'Gatos',
+    'Qual é a principal forma de comunicação vocal dos gatos?',
+    'O Miado'
+)
+
+criaCartao(
+    'Gatos',
+    'Qual é a parte do corpo dos gatos que, se cortada, causa dor intensa e sangramento?',
+    'As unhas, se cortadas muito curtas, atingem a parte vascularizada'
+)
+
+criaCartao(
+    'Gatos',
+    'Que tipo de caçador são os gatos, ou seja, eles caçam sozinhos ou em grupo??',
+    'São caçadores solitários'
+)
+
+criaCartao(
+    'Gatos',
+    'Quantos dedos um gato geralmente tem nas patas dianteiras e nas patas traseiras, respectivamente??',
+    'Geralmente cinco dedos nas patas dianteiras e quatro nas patas traseiras.'
+)
